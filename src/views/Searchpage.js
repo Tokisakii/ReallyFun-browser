2240586a8cdce363bd270698353218a35b4bac49
gg <1341123145@stu.xjtu.edu.cn>
2022-07-07 17:38:27 +0800

    完善搜索功能，可显示搜索卡片

--- src/views/Searchpage.js
+++ src/views/Searchpage.js
@@ -1,14 +1,37 @@
 import React, { Component } from "react";
 import Container from "@mui/material/Container";
+import axios from "axios";
 import GameList from "../components/GameList";
+import { withParams, withNavigate } from "../utils/RouterTool";
 
-export default class Searchpage extends Component {
-  // const =
+class Searchpage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { games: [] };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://127.0.0.1:4523/m1/1221635-0-default/games", {
+        params: { tag_id: this.props.params.searchParams },
+      })
+      .then(
+        (response) => {
+          this.setState({ games: response.data.data });
+        },
+        (error) => {
+          console.log("fail", error);
+        }
+      );
+  }
+
   render() {
+    const { games } = this.state;
     return (
       <Container component="main" maxWidth="md">
-        {/* <GameList games={games} /> */}
+        <GameList games={games} />
       </Container>
     );
   }
 }
+export default withParams(Searchpage);
